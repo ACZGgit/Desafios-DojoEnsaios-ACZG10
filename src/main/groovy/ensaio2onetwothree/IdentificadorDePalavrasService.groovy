918448b535f9fc260088b172322bfc3504c722e3
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-16 16:59:32 -0300

    feat: iniciando requisito base

--- src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
+++ src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
@@ -5,9 +5,15 @@ class IdentificadorDePalavrasService {
     String execute(String entrada) {
 
         List<String> listaDePalavras = entrada.tokenize(',')
+        // Lógica
+        List<String> certo = ["one", "two", "three"]
+        int countLetrasCorretas = 0
+        for (int i = 1) {
+            
+        }
+
 
         return listaDePalavras
-
     }
 
 }
