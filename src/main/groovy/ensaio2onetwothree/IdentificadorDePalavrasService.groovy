27cff700b926cf1d0f879d1085543f98fd3ce439
AlanF-Oliveira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-09-16 18:30:27 -0300

    feat: alan ferreira

--- src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
+++ src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
@@ -7,21 +7,21 @@ class IdentificadorDePalavrasService {
         List<String> listaDePalavras = entrada.tokenize(',')
         // Lógica
         List<String> certo = ["one", "two", "three"]
-        int[] certoContagem = int[2] // [0, 0, 0]
+        List<Integer> certoContagem = [0, 0, 0]
 
         List<String> resultado = []
-        listaDePalavras.remove(0)
+        //listaDePalavras.remove(0)
         for (palavra in listaDePalavras) {
             int contadorIndex = 0
 
             //percorrendo caracteres
             for (c in palavra) {
-                if (c == palavra(contadorIndex)) {
+                if (c == certo[contadorIndex]) {
                     int indexContagem = listaDePalavras.indexOf(palavra)
                     certoContagem[indexContagem]++
                 }
+                contadorIndex++
             }
-            contadorIndex++
         }
 
 
