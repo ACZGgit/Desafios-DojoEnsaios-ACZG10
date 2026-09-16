02d450d81b1e8aa3ed7e134a22db37a3acb0cf41
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-16 17:36:49 -0300

    feat: sei la

--- src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
+++ src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
@@ -7,14 +7,28 @@ class IdentificadorDePalavrasService {
         List<String> listaDePalavras = entrada.tokenize(',')
         // Lógica
         List<String> certo = ["one", "two", "three"]
+        List<String> resultado
 
         for (palavra in listaDePalavras) {
             int countLetrasCorretas = 0
             for (c in palavra) {
+                for (palavraCerta in certo) {
+                    for (c2 in palavraCerta) {
+                        if (c == c2) {
+                            countLetrasCorretas++
 
+                        }
+                        if (countLetrasCorretas = palavraCerta.length() - 1) {
+                            if (palavraCerta.equals("one")) {
+                                
+                            }
+                        }
+
+                    }
                 }
             }
         }
+    }
 
 
 
