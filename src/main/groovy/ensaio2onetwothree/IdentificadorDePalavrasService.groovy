12380607a0b652dcd130f8a7cda7052de05a7412
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-16 19:26:04 -0300

    feat: quase lá

--- src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
+++ src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
@@ -9,24 +9,45 @@ class IdentificadorDePalavrasService {
         List<String> certo = ["one", "two", "three"]
         List<Integer> certoContagem = [0, 0, 0]
 
-        List<String> resultado = []
+        String resultado = ""
         //listaDePalavras.remove(0)
+        listaDePalavras.remove(0)
+        int numeroFinal
         for (palavra in listaDePalavras) {
             int contadorIndex = 0
+            certoContagem = [0, 0, 0]
 
             //percorrendo caracteres
             for (c in palavra) {
-                String tempLetra =certo[contadorIndex][0]
                 if (c == certo[contadorIndex][0]) {
                     int indexContagem = listaDePalavras.indexOf(palavra)
                     certoContagem[indexContagem]++
                 }
+
                 contadorIndex++
             }
+            int valorMaximo = certoContagem.max()
+            int indiceValorMaximo = certoContagem.indexOf(valorMaximo)
+            numeroFinal =  convertePalavraEmNumero(certo[indiceValorMaximo])
+            resultado+="${numeroFinal}"
         }
+        return resultado.join(",")
+    }
 
-
-        return resultado.join(",");
+    int convertePalavraEmNumero(String palavra) {
+        switch (palavra) {
+            case "one":
+                return 1
+                break
+            case "two":
+                return 2
+                break
+            case "three":
+                return 3
+                break
+            default:
+                break
+        }
     }
 
 }
