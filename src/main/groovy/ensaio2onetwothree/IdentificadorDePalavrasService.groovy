861e2ca0f25f9318383dc1f9b18f3664a02304d4
Athus27 <athussilva27@gmail.com>
2026-09-16 17:59:53 -0300

    feat: percorrendo caracteres e comparando com referencias

--- src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
+++ src/main/groovy/ensaio2onetwothree/IdentificadorDePalavrasService.groovy
@@ -7,33 +7,24 @@ class IdentificadorDePalavrasService {
         List<String> listaDePalavras = entrada.tokenize(',')
         // Lógica
         List<String> certo = ["one", "two", "three"]
+        List<Integer> certoContagem = [0, 0, 0]
+
         List<String> resultado = []
         listaDePalavras.remove(0)
         for (palavra in listaDePalavras) {
             int countLetrasCorretas = 0
-            for (c in listaDePalavras) {
-                int contadorIndex =0
-                for (palavraCerta in certo) {
+            int contadorIndex = 0
 
-                    if(c == palavraCerta(contadorIndex))
-                        countLetrasCorretas++
-
-                    if (countLetrasCorretas = palavraCerta.length() - 1) {
-                        if (palavraCerta.equals("one")) {
-                            resultado << "1"
-                        }
-                        if (palavraCerta.equals("two")) {
-                            resultado << "2"
-                        }
-                        if (palavraCerta.equals("three")) {
-                            resultado << "3"
-                        }
-                    }
-
-
-                }
+            //percorrendo caracteres
+            for (c in palavra) {
+                if (c==palavra(contadorIndex))
+                    countLetrasCorretas++
+                    int indexContagem = listaDePalavras.indexOf(palavra)
+                    certoContagem[indexContagem] =certoContagem(indexContagem)+1;
             }
         }
+
+
         return resultado.join(",");
     }
 
